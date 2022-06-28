a9c6efef5f563be913bf9ca0cd77a93412a421d6
rgzntrade <309618485@qq.com>
2022-06-28 12:52:16 +0800

    feat：1.添加packer插件管理工具

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,5 @@
 -- nvim基础配置
 require('basic')
+-- 插件管理列表
+require('plugins')
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,54 @@
+local fn = vim.fn
+
+-- 自动安装 packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+vim.cmd,
+[[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]]
+)
+
+-- packer 安装准备就绪前直接返回
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+packer.init {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}
+
+packer.startup({function()
+  -- Packer 可以管理自己本身
+  use 'wbthomason/packer.nvim'
+  -- 插件列表...
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end
+})
