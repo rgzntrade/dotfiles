2365dbc1f1d9cc9e5ee8a2f86aeb9b2e7620ebe7
rgzntrade <309618485@qq.com>
2022-07-26 22:02:26 +0800

    feat：1.取消回车默认选中第一个

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -50,7 +50,7 @@ cmp.setup({
     ["<C-j>"] = cmp.mapping.select_next_item(),
     -- 确认
     ["<CR>"] = cmp.mapping.confirm({
-      select = true,
+      select = false,
       behavior = cmp.ConfirmBehavior.Replace
     }),
     -- 如果窗口内容太多，可以滚动
