7246fc573727cb1392093ca6661a20129c7a57ed
rgzntrade <309618485@qq.com>
2022-07-26 10:43:06 +0800

    feat：1.开启rainbow，但不起作用。

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -34,6 +34,16 @@ treesitter.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+ -- 彩虹括号
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
 -- 启用增量选择模块
 incremental_selection = {
   enable = true,
