6d671c293ef5ab75c33f316b043a24d96b6a44a2
rgzntrade <309618485@qq.com>
2022-08-08 19:45:09 +0800

    feat：1.不显示空格

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -68,7 +68,7 @@ vim.opt.termguicolors = true
 -- 不可见字符的显示，这里只把空格显示为一个点
 vim.o.list = true
 
-vim.o.listchars = "space:⋅,tab:→ ,trail:-,precedes:«,extends:…,nbsp:+,eol:⏎"
+vim.o.listchars = "space: ,tab:→ ,trail:-,precedes:«,extends:…,nbsp:+,eol:⏎"
 -- 补全增强
 vim.o.wildmenu = true
 -- Dont' pass messages to |ins-completin menu|
