301b08a1d83d416d01d398e7e4dd3979d59cf9c2
rgzntrade <309618485@qq.com>
2023-05-24 20:46:38 +0800

    增加which-key绑定，作用未知

--- nvim/lua/conf/legendary.lua
+++ nvim/lua/conf/legendary.lua
@@ -92,6 +92,21 @@ legendary.setup({
       -- autocmds here
     },
   },
+  -- which-key
+  which_key = {
+    -- Automatically add which-key tables to legendary
+    -- see ./doc/WHICH_KEY.md for more details
+    auto_register = true,
+    -- you can put which-key.nvim tables here,
+    -- or alternatively have them auto-register,
+    -- see ./doc/WHICH_KEY.md
+    mappings = {},
+    opts = {},
+    -- controls whether legendary.nvim actually binds they keymaps,
+    -- or if you want to let which-key.nvim handle the bindings.
+    -- if not passed, true by default
+    do_binding = true,
+  },
   -- load extensions
   extensions = {
     -- load keymaps and commands from nvim-tree.lua
