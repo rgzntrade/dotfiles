c48a6887d138fab301eee683c5467b3f4ba61440
rgzntrade <309618485@qq.com>
2023-05-27 13:30:07 +0800

    修改dotbot配置，未验证

--- install.ps1
+++ install.ps1
@@ -6,17 +6,64 @@ $DOTBOT_DIR = "dotbot"
 $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = $PSScriptRoot
 
+# 安装 Chocolatey
+Write-Host "Installing Chocolatey..."
+if (!(Test-Path -Path "$env:ProgramData\chocolatey\bin\choco.exe")) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    Write-Host "Chocolatey is already installed. Skipping."
+}
+
 Set-Location $BASEDIR
 
-Set-Location $DOTBOT_DIR && git submodule update --init --recursive
+git submodule update --init --recursive
+
 Set-Location $BASEDIR
-foreach ($PYTHON in ('python3', 'python', 'python2')) {
+$pythonFound = $false
+$pythonCmd = $null
+$pythonVersions = @('python3', 'python', 'python2')
+
+foreach ($PYTHON in $pythonVersions) {
     # Python redirects to Microsoft Store in Windows 10 when not installed
     if (& { $ErrorActionPreference = "SilentlyContinue"
             ![string]::IsNullOrEmpty((&$PYTHON -V))
             $ErrorActionPreference = "Stop" }) {
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
-        return
+        $pythonCmd = $PYTHON
+        $pythonFound = $true
+        break
     }
 }
-Write-Error "Error: Cannot find Python."
+
+if ($pythonFound) {
+    &$pythonCmd $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
+
+    # 安装 cmake
+    Write-Host "Installing cmake..."
+    if (!(Test-Path -Path "$(Get-Command cmake -ErrorAction SilentlyContinue).Source")) {
+        choco install cmake
+    } else {
+        Write-Host "cmake is already installed. Skipping."
+    }
+
+    # 安装 bob
+    Write-Host "Installing bob..."
+    if (!(Test-Path -Path "$(Get-Command bob -ErrorAction SilentlyContinue).Source")) {
+        choco install bob
+    } else {
+        Write-Host "bob is already installed. Skipping."
+    }
+
+    # 使用 bob 安装最新版 nvim
+    Write-Host "Installing latest nvim..."
+    if (Test-Path -Path "$(Get-Command bob -ErrorAction SilentlyContinue).Source") {
+        bob install nvim
+    } else {
+        Write-Host "bob is not installed. Skipping nvim installation."
+    }
+
+    Write-Host "Installation completed."
+} else {
+    Write-Error "Error: Cannot find Python."
+}
+
--- install.sh
+++ install.sh
@@ -8,8 +8,79 @@ DOTBOT_DIR="dotbot"
 DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# 更新自仓库
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${DOTBOT_DIR}"
+# git submodule update --init --recursive "${DOTBOT_DIR}"
+git submodule update --init --recursive 
 
+
+
+# 安装 cmake
+echo "Installing cmake..."
+if command -v cmake >/dev/null 2>&1; then
+  echo "cmake is already installed. Skipping."
+else
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install -y cmake
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install cmake
+  fi
+fi
+
+# 安装 zsh
+echo "Installing zsh..."
+if command -v zsh >/dev/null 2>&1; then
+  echo "zsh is already installed. Skipping."
+else
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install -y zsh
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install zsh
+  fi
+fi
+
+# 安装 tmux
+echo "Installing tmux..."
+if command -v tmux >/dev/null 2>&1; then
+  echo "tmux is already installed. Skipping."
+else
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install -y tmux
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install tmux
+  fi
+fi
+
+# 安装 bob
+echo "Installing bob..."
+if command -v bob >/dev/null 2>&1; then
+  echo "bob is already installed. Skipping."
+else
+  if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+    if ! command -v curl >/dev/null 2>&1; then
+      echo "curl is not installed. Installing curl..."
+      if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo apt-get install -y curl
+      elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install curl
+      fi
+    fi
+    bash <(curl -sL https://github.com/ripper2hl/bob/raw/master/install.sh)
+  else
+    echo "Unsupported operating system: $OSTYPE. Skipping bob installation."
+  fi
+fi
+
+# 使用 bob 安装最新版 nvim
+echo "Installing latest nvim..."
+if command -v bob >/dev/null 2>&1; then
+  bob install nvim
+else
+  echo "bob is not installed. Skipping nvim installation."
+fi
+
+echo "Installation completed."
+
+# 使用dotbot配置
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
