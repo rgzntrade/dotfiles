439b0904843b41bde52b8146d4ca5b0df5e2db20
rgzntrade <309618485@qq.com>
2023-06-13 20:41:43 +0800

    添加up

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 # git submodule update --init --recursive "${DOTBOT_DIR}"
-# git submodule update --init --recursive 
+git submodule update --init --recursive 
 
 
 
